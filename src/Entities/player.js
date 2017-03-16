00ec666142a1f1bbb4dac032af39b20f05131c6a
israelcanul <israel.canul@gmail.com>
2017-03-15 21:41:42 -0600

    se agregan las animaciones del player

--- src/Entities/player.js
+++ src/Entities/player.js
@@ -1,3 +1,5 @@
+
+
  export default class Player{
   constructor(contexto){
     this.contexto = contexto;
@@ -5,26 +7,37 @@
     this.imagen.src = 'img/sprite.png';
     this.spriteWidth = this.imagen.width / 10;
     this.spriteHeight = this.imagen.height / 4;
-    this.scaleImg = 1;
+    this.scaleImg = 0.8;
     this.animacion = [];
     this.crearAnimaciones();
-
+    this.cont= 0;
   }
   dibujar(){
     var ctx = this.contexto
     if(this.contexto){
-      ctx.drawImage(this.imagen,this.animacion[0][0],this.animacion[0][1],this.spriteWidth,this.spriteHeight,0,0,this.getWidth(),this.getHeight());
+      let fotograma = this.animar();
+      this.cont++;// contador de la animacion
+      ctx.drawImage(this.imagen,fotograma[0],fotograma[1],this.spriteWidth,this.spriteHeight,0,0,this.getWidth(),this.getHeight());
     }
   }
   crearAnimaciones(){
+    this.animMoveRight = [[this.spriteWidth * 6,this.spriteHeight * 0],[this.spriteWidth * 7,this.spriteHeight * 0],[this.spriteWidth * 8,this.spriteHeight * 0]];
     this.animMoveLeft = [[this.spriteWidth * 6,this.spriteHeight * 0],[this.spriteWidth * 7,this.spriteHeight * 0],[this.spriteWidth * 8,this.spriteHeight * 0]];
-    this.animStop = [[this.spriteWidth * 0,this.spriteHeight * 0],[this.spriteWidth * 0,this.spriteHeight * 1],[this.spriteWidth * 1,this.spriteHeight * 2]];
-    this.animacion = this.animStop;
+    this.animStop = [[this.spriteWidth * 0,this.spriteHeight * 0],[this.spriteWidth * 0,this.spriteHeight * 0],[this.spriteWidth * 1,this.spriteHeight * 0],[this.spriteWidth * 1,this.spriteHeight * 0],[this.spriteWidth * 2,this.spriteHeight * 0],[this.spriteWidth * 2,this.spriteHeight * 0]];
+    this.animMoveUp = [[this.spriteWidth * 6,this.spriteHeight * 0],[this.spriteWidth * 7,this.spriteHeight * 0],[this.spriteWidth * 8,this.spriteHeight * 0]];
+    this.animMoveDown = [[this.spriteWidth * 6,this.spriteHeight * 0],[this.spriteWidth * 7,this.spriteHeight * 0],[this.spriteWidth * 8,this.spriteHeight * 0]];
+    this.animacion = this.animMoveLeft;
   }
   getWidth(){
     return this.scaleImg * this.spriteWidth;
   }
   getHeight(){
     return this.scaleImg * this.spriteHeight;
+  }
+  animar(){
+    if(this.cont == this.animacion.length){
+      this.cont = 0;
+    }
+    return [this.animacion[this.cont][0],this.animacion[this.cont][1]];
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 window.onload = inicio;
 function inicio(){
   var juego = new Juego();
-  var temporizador = setInterval(function(){juego.controlLoop()},25);
+  var temporizador = setInterval(function(){juego.controlLoop()},60);
 }
 
 
@@ -12,20 +12,10 @@ class Juego{
   constructor(){
     this.canvas = document.getElementById('canvas');
     this.contexto = this.canvas.getContext('2d');
-    this.canvas.width=800;
-    this.canvas.height=600;
+    this.canvas.width=400;
+    this.canvas.height=200;
     this.gameRunning = true;
-    //this.canvas.style.backgroundColor="black";
-    //if(this.canvas.getContext){
-    //   var ctx = this.canvas.getContext('2d');
-    //   ctx.fillRect(25,25,100,100);
-    //   ctx.clearRect(45,45,60,60);
-    //   ctx.fillRect(50,50,50,50);
-    // }
-    //this.temporizador = setInterval(this.loop(),25);
-    //this.iniciar();
-
-
+    this.canvas.style.backgroundColor="black";
 
     // creamos el player
     this.player = new Player(this.contexto);
@@ -47,10 +37,10 @@ class Juego{
   }
   loop(){
     // calculamos el tiempo delta
-    var delta=(new Date().getTime()) - this.tiempoTranscurrido;
+    var delta=Math.min(1000/60 ,(new Date().getTime()) - this.tiempoTranscurrido)/1000;
     this.tiempoTranscurrido=new Date().getTime();
     this.contexto.clearRect(0,0,this.canvas.width,this.canvas.height);
-    //dibujamos al jugador 
+    //dibujamos al jugador
     this.player.dibujar();
   }
 
